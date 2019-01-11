a9e0de71f2734502ec887daf3c9e676fef388875
Justin Langston <justin@bitpay.com>
2019-01-11 10:17:08 -0500

    feat(storage): add bulkImport method

--- packages/bitcore-node/src/models/base.ts
+++ packages/bitcore-node/src/models/base.ts
@@ -1,5 +1,7 @@
 import { Storage } from '../services/storage';
 import { ObjectID, Collection, MongoClient, Db } from 'mongodb';
+import { Writable, Readable } from 'stream';
+import { partition } from '../utils/partition';
 
 export type MongoBound<T> = T & Partial<{ _id: ObjectID }>;
 export abstract class BaseModel<T> {
@@ -42,5 +44,44 @@ export abstract class BaseModel<T> {
     } else {
       throw new Error('Not connected to the database yet');
     }
+  }
+
+  async bulkImport(ops: Array<any>, partitionSize: number): Promise<any> {
+    const collection = this.collection;
+    let opIndex = 0;
+    ops = partition(ops, partitionSize);
+    const opInputStream = new Readable({
+      objectMode: true,
+      read() {
+        if (opIndex < ops.length) {
+          this.push(ops[opIndex]);
+          opIndex++;
+        }
+        else {
+          this.push(null);
+        }
+      }
+    });
+
+    const bulkWriter = new Writable({
+      objectMode: true,
+      async write(chunk, _, callback) {
+        try {
+          await collection.bulkWrite(chunk);
+          callback();
+        } catch (err) {
+          callback(err);
+        }
+      }
+    });
+
+    return new Promise((resolve, reject) => {
+      bulkWriter.on('unpipe', resolve);
+      bulkWriter.on('error', (err) => {
+        bulkWriter.destroy();
+        reject(err);
+      });
+      opInputStream.pipe(bulkWriter);
+    });
   }
 }
